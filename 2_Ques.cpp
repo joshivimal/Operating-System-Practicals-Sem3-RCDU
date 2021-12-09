f210acf9fb612e80f8000e9ad212aeeef83bca1e
itsVimal <vimaljoshi8860@gmail.com>
2021-12-09 10:46:14 +0530

    commited the code to push to the repo

---
+++ 2_Ques.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <sys/wait.h>
+#include <unistd.h> 
+using namespace std;
+int main()
+{
+    pid_t id = fork();
+{
+    cout<<"\n Child Terminated";
+}
+
+{
+    wait(NULL);
+    cout<<"\n Parent Terminated";
+}
+return 0;
+}
